40331b900fd7cff5b85dc44e083949d5e088f082
Sourav974 <souravyadav97@gmail.com>
2025-01-01 02:38:08 +0530

    fix the supertest version issue and implement the persisitng the user test case

--- src/tests/users/register.spec.ts
+++ src/tests/users/register.spec.ts
@@ -1,7 +1,28 @@
 import request from "supertest";
-import app from "../../src/app";
+import app from "../../app";
+
+import { DataSource } from "typeorm";
+import { AppDataSource } from "../../config/data-source";
+import { User } from "../../entity/User";
+import { truncateTables } from "../utils";
 
 describe("POST auth/register", () => {
+    let connection: DataSource;
+
+    beforeAll(async () => {
+        connection = await AppDataSource.initialize();
+    });
+
+    beforeEach(async () => {
+        await truncateTables(connection);
+    });
+
+    afterAll(async () => {
+        if (connection && typeof connection.destroy === "function") {
+            await connection.destroy();
+        }
+    });
+
     describe("Given all Filelds", () => {
         it("should return 201 status code", async () => {
             // AAA
@@ -47,6 +68,27 @@ describe("POST auth/register", () => {
             expect(response.headers["content-type"]).toEqual(
                 expect.stringContaining("json"),
             );
+        });
+
+        it("should persist the user in the database", async () => {
+            // Arrange
+
+            const userData = {
+                firstName: "Sourav",
+                lastName: "Yadav",
+                email: "sourav.mern.space",
+                password: "secret",
+            };
+
+            // Act
+
+            await request(app).post("/auth/register").send(userData);
+
+            // Assert
+
+            const userRepository = connection.getRepository(User);
+            const users = await userRepository.find();
+            expect(users).toHaveLength(1);
         });
     });
 
