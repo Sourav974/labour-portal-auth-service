68a0248495f02aa1f0fc6355a9176f8d584e4e10
Sourav974 <souravyadav97@gmail.com>
2025-01-02 11:47:15 +0530

    add error handling and logging

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -2,15 +2,24 @@ import { Repository } from "typeorm";
 // import { AppDataSource } from "../config/data-source";
 import { User } from "../entity/User";
 import { userData } from "../types";
+import createHttpError from "http-errors";
 
 export class UserService {
     constructor(private userRepository: Repository<User>) {}
     async create({ firstName, lastName, email, password }: userData) {
-        await this.userRepository.save({
-            firstName,
-            lastName,
-            email,
-            password,
-        });
+        try {
+            return await this.userRepository.save({
+                firstName,
+                lastName,
+                email,
+                password,
+            });
+        } catch (err) {
+            const error = createHttpError(
+                500,
+                "Failed store data in the database",
+            );
+            throw error;
+        }
     }
 }
