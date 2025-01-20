42caedd004c6e3a00e0e2b8ce535d076d5469a60
Sourav974 <souravyadav97@gmail.com>
2025-01-20 14:36:11 +0530

    fix logger timestamp

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -170,7 +170,7 @@ export class AuthController {
 
             this.logger.info("User has been logged in", { id: user.id });
 
-            res.status(200).json({ id: user.id });
+            res.json({ id: user.id });
         } catch (error) {
             next(error);
             return;
