eb88055c46391585cbf54f30f80976c8a0a61ac5
Sourav974 <souravyadav97@gmail.com>
2025-02-15 21:39:04 +0530

    change private key read strategy

--- src/services/TokenService.ts
+++ src/services/TokenService.ts
@@ -1,6 +1,3 @@
-import fs from "fs";
-import path from "path";
-
 import { JwtPayload, sign } from "jsonwebtoken";
 import createHttpError from "http-errors";
 import { Config } from "../config";
@@ -13,12 +10,15 @@ export class TokenService {
     constructor(private refreshTokenRepository: Repository<RefreshToken>) {}
 
     generateAccessToken(payload: JwtPayload) {
-        let privateKey: Buffer;
+        let privateKey: string;
+        if (!Config.PRIVATE_KEY) {
+            const error = createHttpError(500, "SECRET_KEY os not set");
+
+            throw error;
+        }
 
         try {
-            privateKey = fs.readFileSync(
-                path.join(__dirname, "../../certs/private.pem"),
-            );
+            privateKey = Config.PRIVATE_KEY;
         } catch (error) {
             const err = createHttpError(500, "Error while reading private key");
 
